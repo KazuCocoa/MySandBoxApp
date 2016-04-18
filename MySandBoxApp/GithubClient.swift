e179e30d1b602b6f0149e7d0003a11c804e384da
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-04-19 01:34:26 +0900

    establish sending request and update view

--- MySandBoxApp/GithubClient.swift
+++ MySandBoxApp/GithubClient.swift
@@ -21,24 +21,6 @@ struct GitHubGist: Decodable {
     }
 }
 
-class GithubClient {
-
+struct GithubClient {
     let githuGist = "https://api.github.com/users/KazuCocoa/gists"
-
-    func getPublicGist(user: String) {
-        Alamofire.request(.GET, githuGist)
-            .responseJSON { response in
-                // Example:
-                // [MyHttpBinClient]: ip is Optional("119.31.155.178")
-                // ========== Optional(MySandBoxApp.GitHubGist(url: "https://api.github.com/gists/344c9f4730aa30d5510c", id: "344c9f4730aa30d5510c", publicGist: true, owner: "KazuCocoa"))
-                // ========== Optional(MySandBoxApp.GitHubGist(url: "https://api.github.com/gists/1a0a1d712c2baecccad7", id: "1a0a1d712c2baecccad7", publicGist: true, owner: "KazuCocoa"))
-                // ========== Optional(MySandBoxApp.GitHubGist(url: "https://api.github.com/gists/26a5f3ee8b9bb19aafb5", id: "26a5f3ee8b9bb19aafb5", publicGist: true, owner: "KazuCocoa"))
-                if let JSON = response.result.value as? Array<Dictionary<String, AnyObject>> {
-                    JSON.forEach { json in
-                        let result = try? GitHubGist.decodeValue(json)
-                        print("========== \(result)")
-                    }
-                }
-        }
-    }
 }
--- MySandBoxApp/ViewController.swift
+++ MySandBoxApp/ViewController.swift
@@ -1,14 +1,38 @@
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gistLits: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        gistLits.text = "viewDidLoad\n"
+    }
+
+    override func viewWillAppear(animated: Bool) {
+
         let client = MyHttpBinClient()
         client.getHttpBinGet()
 
-        let githubClient = GithubClient()
-        githubClient.getPublicGist("KazuCocoa")
+        getGistAndUpdateView()
+    }
+
+    func getGistAndUpdateView() {
+        Alamofire.request(.GET, GithubClient().githuGist)
+            .responseJSON { response in
+                if let JSON = response.result.value as? Array<Dictionary<String, AnyObject>> {
+                    JSON.forEach { json in
+                        let result = try? GitHubGist.decodeValue(json)
+                        self.gistLits.text?.appendContentsOf("url: " + (result?.url)! + "\n")
+                        self.gistLits.text?.appendContentsOf("id: " + (result?.id)! + "\n")
+                        self.gistLits.text?.appendContentsOf("owner: " + (result?.owner)! + "\n\n")
+
+                    }
+                }
+        }
     }
 
     override func didReceiveMemoryWarning() {
